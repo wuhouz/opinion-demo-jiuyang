f41d2ba70fd04bd276ff0e0f9828908c72f7d221
suzhen.han <suzhen.han@ifchange.com>
2018-08-28 20:15:22 +0800

    添加九阳坐标小点点数据

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import Opinion from './OpinionDemo';
+import 'antd/dist/antd.css';
+
+class App extends Component {
+  render() {
+    return <Opinion />;
+  }
+}
+
+export default App;
---
+++ src/OpinionDemo/Employer/peerData/PeerDesciptionData.js
@@ -0,0 +1,47 @@
+const DesData = [
+    {
+        name: 'A',
+        isActive: false,
+        title: '行业普遍优势',
+        slogan: '',
+        desc: '该区域关注度最高的行业雇主特征为：',
+        list: ['优化', '改进'],
+        advantage: [
+            {
+                suggest: '1、行业内企业普遍会采取开放借鉴的心态，学习优化企业组织结构。\n' +
+                    '2、员工对于晋升机制较看重。行业内企业在这点上普遍做的比较好，也得到候选人积极的认可。'
+
+            }
+        ]
+    },
+    {
+        name: 'B',
+        isActive: false,
+        title: '竞争机会1',
+        slogan: '同行独占的优势特征',
+        advantage: []
+    },
+    {
+        name: 'C',
+        isActive: false,
+        title: '竞争机会2',
+        slogan: '同行独占的负面特征',
+        advantage: []
+    },
+    {
+        name: 'D',
+        isActive: false,
+        title: '行业普遍问题',
+        slogan: '',
+        desc: '该区域关注度最高的行业雇主特征为：',
+        list: ['伤害', '违约'],
+        advantage: [
+            {
+                suggest: '1、由于行业自身特征，"项目质量引起的企业危机"一直是所有候选人较关心的。建议在这方面行业企业都做好严格的质量把控，提高在候选人心目中的信任度。\n' +
+                '2、注重企业危机公关处理。这个问题是该行业所有企业都面临的，建议24小时监控企业动态舆情，及时将危机舆情控制在信息源头。'
+            }
+        ]
+    }
+];
+
+export default DesData;
